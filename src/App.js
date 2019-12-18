03cf3dac8c56602c0b51f1a10ae6289c828de987
charcharmasonjar <charlotte.dibb@gmail.com>
2019-12-17 19:44:05 -0800

    Add skills section

--- src/App.js
+++ src/App.js
@@ -1,26 +1,137 @@
-import React from "react";
+import React, { useState } from "react";
 import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons";
 import "./App.css";
 import "./landing.css";
 import "./projects.css";
-// import waves from "./waves.mp4";
+import "./skills.css";
 import ocean from "./ocean_tall.mp4";
-import { FaAngleDoubleDown } from "react-icons/fa";
+import {
+  FaAngleDoubleDown,
+  FaBootstrap,
+  FaCss3Alt,
+  FaDatabase,
+  FaGem,
+  FaGitAlt,
+  FaHtml5,
+  FaJsSquare,
+  FaLeaf,
+  FaNode,
+  FaReact,
+  // FaServer,
+  FaCcStripe,
+  FaSms
+} from "react-icons/fa";
 
 function App() {
+  const [skills, updateSkills] = useState("hide");
+  const [project, updateProject] = useState({
+    js: "off",
+    ruby: "off",
+    html: "off",
+    css: "off",
+    bootStrap: "off",
+    react: "off",
+    postgres: "off",
+    mongo: "off",
+    git: "off",
+    node: "off",
+    stripe: "off",
+    twilio: "off"
+  });
+
+  const mouseEnter = function(project) {
+    if (project === "xstitch") {
+      updateProject({
+        js: "on",
+        ruby: "on",
+        html: "on",
+        css: "on",
+        bootStrap: "on",
+        react: "on",
+        postgres: "on",
+        mongo: "off",
+        git: "on",
+        node: "off",
+        stripe: "off",
+        twilio: "off"
+      });
+    } else if (project === "hungry") {
+      updateProject({
+        js: "on",
+        ruby: "off",
+        html: "on",
+        css: "on",
+        bootStrap: "on",
+        react: "off",
+        postgres: "on",
+        mongo: "off",
+        git: "on",
+        node: "on",
+        stripe: "off",
+        twilio: "on"
+      });
+    } else if (project === "tweeter") {
+      updateProject({
+        js: "on",
+        ruby: "off",
+        html: "on",
+        css: "on",
+        bootStrap: "off",
+        react: "off",
+        postgres: "off",
+        mongo: "on",
+        git: "on",
+        node: "on",
+        stripe: "off",
+        twilio: "off"
+      });
+    } else if (project === "jungle") {
+      updateProject({
+        js: "on",
+        ruby: "on",
+        html: "on",
+        css: "on",
+        bootStrap: "on",
+        react: "off",
+        postgres: "on",
+        mongo: "off",
+        git: "on",
+        node: "off",
+        stripe: "on",
+        twilio: "off"
+      });
+    } else if (project === "scheduler") {
+      updateProject({
+        js: "on",
+        ruby: "off",
+        html: "on",
+        css: "on",
+        bootStrap: "off",
+        react: "on",
+        postgres: "off",
+        mongo: "off",
+        git: "on",
+        node: "off",
+        stripe: "off",
+        twilio: "off"
+      });
+    }
+  };
+
+  const skillsShow = function() {
+    updateSkills("show");
+  };
+
+  const skillsHide = function() {
+    updateSkills("hide");
+  };
+
   return (
     <div className="App">
       <Parallax pages={5} style={{ background: "#fda4ff" }}>
         <ParallaxLayer offset={0} speed={0}>
           <div className="hero-vid-container">
-            <video
-              className="hero-vid"
-              autoPlay
-              loop
-              muted
-              // style={{ width: "100vw" }}
-              // style={{ height: "100%" }}
-            >
+            <video className="hero-vid" autoPlay loop muted>
               <source src={ocean} type="video/mp4" />
             </video>
           </div>
@@ -45,7 +156,7 @@ function App() {
         <ParallaxLayer offset={0.99} speed={0.3}>
           <div className="angle">
             <h2>
-              I'm Charlotte. I'm a full stack web developer based in Vancouver,
+              I'm Charlotte. I'm a fullstack web developer based in Vancouver,
               BC.
             </h2>
           </div>
@@ -53,155 +164,182 @@ function App() {
         <ParallaxLayer offset={1} speed={0.2}>
           <div className="info">
             <p className="summary">
-              I recently graduated from the Web Development Bootcamp at
-              Lighthouse Labs. My background is in Human Resources and I have a
-              BA in Psychology from McGill University.
+              I’m a McGill University psychology grad with a past life in Human Resources. Seeking new and more interesting challenges, I took
+              the leap into programming and web development. I love making
+              beautiful, robust UIs for broad audiences, solving tricky
+              problems, and seeing my applications come to life. Aside from
+              coding, I enjoy cross stitching, making 3D images and animations in
+              Blender, and reading sci-fi novels.
             </p>
           </div>
         </ParallaxLayer>
-        {/* <ParallaxLayer offset={1.5} speed={0.1}>
-          <div className="angle-baby">
-          </div>
-        </ParallaxLayer> */}
-        {/* <ParallaxLayer offset={1.5} speed={1}>
-          <h2>Skills</h2>
-          <table>
-            <tr>
-              <th>LANGUAGES</th>
-              <th>
-                LIBRARIES <br></br>& FRAMEWORKS
-              </th>
-              <th>
-                DATABASES <br></br>& VERSION CONTROL
-              </th>
-            </tr>
-            <tr>
-              <td>JavaScript</td>
-              <td>NodeJs</td>
-              <td>PostgresQL</td>
-            </tr>
-            <tr>
-              <td>Ruby</td>
-              <td>ReactJs</td>
-              <td>Git</td>
-            </tr>
-            <tr>
-              <td>HTML</td>
-              <td>Ajax</td>
-            </tr>
-            <tr>
-              <td>CSS</td>
-              <td>Express</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td>jQuery</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td>Bootstrap</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td>Webpack Dev Server</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td>Rails</td>
-              <td></td>
-            </tr>
-          </table>
-        </ParallaxLayer> */}
-        <ParallaxLayer offset={2.75} speed={0.1}>
-          <div className="projects-divider"></div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={2.9} speed={0.2}>
-          <div className="projects">
-            <h1 className="glowOn">Projects</h1>
-            <div className="project-card">
-              <div className="project-title-gif">
-                <img
-                  src={require("./tweet.gif")}
-                  alt="gif of creating a tweet"
-                />
+        <ParallaxLayer offset={2.75} speed={0.3}>
+          <div className="projects-divider">
+            <div
+              className="projects"
+              onMouseEnter={() => skillsShow()}
+              onMouseLeave={() => skillsHide()}
+            >
+              <h2 className="projects-header">Projects</h2>
+              <div
+                className="project-card"
+                onMouseEnter={() => mouseEnter("xstitch")}
+              >
+                <div className="project-title-gif">
+                  <img
+                    src={require("./gifs/edit.gif")}
+                    alt="gif of editing a pixel art pattern"
+                  />
+                </div>
+                <div className="project-description">
+                  <h2>XStitch</h2>
+                  <p>
+                    A cross-stitch pattern creating app with version history.
+                    Users can create patterns, return to previous saved
+                    versions, and generate pdfs to print out their patterns.
+                    Built with React and Rails. ReactJS, Ruby on Rails, JS,
+                    PostgreSQL, Axios
+                  </p>
+                </div>
               </div>
-              <div className="project-description">
-                <h2>Tweeter</h2>
-                <p>
-                  Tweeter is a simple, single-page Twitter clone. Front end was
-                  built with HTML, CSS, JS, jQuery and AJAX. Back end was built
-                  using Node, Express and MongoDB.
-                </p>
+              <div
+                className="project-card"
+                onMouseEnter={() => mouseEnter("hungry")}
+              >
+                <div className="project-title-gif">
+                  <img
+                    src={require("./gifs/hungry_heroes_checkout.gif")}
+                    alt="loading..."
+                  />
+                </div>
+                <div className="project-description">
+                  <h2>Hungry Heroes</h2>
+                  <p>
+                    A food pick-up ordering app built as a midterm project at
+                    Lighthouse Labs. Users can place orders, which notifies the
+                    restaurant via text message. The restaurant can respond with
+                    estimated wait time and notify the user when the order is
+                    ready for pickup.
+                    <br />
+                    Front-end built using HTML/CSS/Bootstrap, Javascript, AJAX.
+                    Back-end was built using Node.js, Express, and PostreSQL
+                    <br />
+                    Twilio API was used for sending text messages.
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="project-card">
-              <div className="project-title-gif">
-                <img
-                  src={require("./edit.gif")}
-                  alt="gif of editing a pixel art pattern"
-                />
+              <div
+                className="project-card"
+                onMouseEnter={() => mouseEnter("tweeter")}
+              >
+                <div className="project-title-gif">
+                  <img
+                    src={require("./gifs/tweet.gif")}
+                    alt="gif of creating a tweet"
+                  />
+                </div>
+                <div className="project-description">
+                  <h2>Tweeter</h2>
+                  <p>
+                    Tweeter is a simple, single-page Twitter clone. Front end
+                    was built with HTML, CSS, JS, jQuery and AJAX. Back end was
+                    built using Node, Express and MongoDB.
+                  </p>
+                </div>
               </div>
-              <div className="project-description">
-                <h2>XStitch</h2>
-                <p>
-                  A cross-stitch pattern creating app built as a final project
-                  at Lighthouse Labs. Built with React and Rails.
-                </p>
+              <div
+                className="project-card"
+                onMouseEnter={() => mouseEnter("jungle")}
+              >
+                <div className="project-title-gif">
+                  <img
+                    src={require("./gifs/jungle_checkout.gif")}
+                    alt="git of user checking out"
+                  />
+                </div>
+                <div className="project-description">
+                  <h2>Jungle</h2>
+                  <p>
+                    Jungle is a mini e-commerce application built with Rails
+                    4.2.
+                    <br />
+                    PostgreSQL, Stripe, Bootstrap
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="project-card">
-              <div className="project-title-gif">
-                <img
-                  src={require("./hungry_heroes_checkout.gif")}
-                  alt="loading..."
-                />
-              </div>
-              <div className="project-description">
-                <h2>Hungry Heroes</h2>
-                <p>
-                  A food pick-up ordering app built as a midterm project at
-                  Lighthouse Labs. Front-end was built using HTML/CSS/Bootstrap,
-                  Javascript, AJAX. Back-end was built using Node.js, Express,
-                  and PostgreSQL. Twilio API was used for sending text messages.
-                </p>
-              </div>
-            </div>
-            <div className="project-card">
-              <div className="project-title-gif">
-                <img
-                  src={require("./jungle_checkout.gif")}
-                  alt="git of user checking out"
-                />
-              </div>
-              <div className="project-description">
-                <h2>Jungle</h2>
-                <p>
-                  Jungle is a mini e-commerce application built with Rails 4.2
-                </p>
-              </div>
-            </div>
-            <div className="project-card">
-              <div className="project-title-gif">
-                <img
-                  src={require("./editAppt.gif")}
-                  alt="git of editing an appointment"
-                />
-              </div>
-              <div className="project-description">
-                <h2>Scheduler</h2>
-                <p>
-                  Scheduler is a single page interview scheduling app built with
-                  React. Storybook, Jest tests, and Cypress were used in the
-                  development of the project.
-                </p>
+              <div
+                className="project-card"
+                onMouseEnter={() => mouseEnter("scheduler")}
+              >
+                <div className="project-title-gif">
+                  <img
+                    src={require("./gifs/editAppt.gif")}
+                    alt="git of editing an appointment"
+                  />
+                </div>
+                <div className="project-description">
+                  <h2>Scheduler</h2>
+                  <p>
+                    Scheduler is a single page interview scheduling app built
+                    with React. Storybook, Jest tests, and Cypress were used in
+                    the development of the project.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </ParallaxLayer>
       </Parallax>
+      <div className={`skills ${skills}`} onMouseEnter={() => skillsShow()}>
+        <div className={`skill-container ${project.js}`}>
+          <FaJsSquare size={35} />
+          JavaScript
+        </div>
+        <div className={`skill-container ${project.ruby}`}>
+          <FaGem size={35} />
+          Ruby
+        </div>
+        <div className={`skill-container ${project.html}`}>
+          <FaHtml5 size={35} />
+          HTML
+        </div>
+        <div className={`skill-container ${project.css}`}>
+          <FaCss3Alt size={35} />
+          CSS
+        </div>
+        <div className={`skill-container ${project.bootStrap}`}>
+          <FaBootstrap size={35} />
+          Bootstrap
+        </div>
+        <div className={`skill-container ${project.react}`}>
+          <FaReact size={35} />
+          ReactJs
+        </div>
+        <div className={`skill-container ${project.postgres}`}>
+          <FaDatabase size={35} />
+          PostgreSQL
+        </div>
+        <div className={`skill-container ${project.mongo}`}>
+          <FaLeaf size={35} />
+          MongoDB
+        </div>
+        <div className={`skill-container ${project.git}`}>
+          <FaGitAlt size={35} />
+          Git
+        </div>
+        <div className={`skill-container ${project.node}`}>
+          <FaNode size={35} />
+          NodeJs
+        </div>
+        <div className={`skill-container ${project.stripe}`}>
+          <FaCcStripe size={35} />
+          Stripe
+        </div>
+        <div className={`skill-container ${project.twilio}`}>
+          <FaSms size={35} />
+          Twilio
+        </div>
+      </div>
     </div>
   );
 }
