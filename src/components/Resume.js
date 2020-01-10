358b8004aba8219f5f03767b52cea5f8b1dfde6c
charcharmasonjar <charlotte.dibb@gmail.com>
2020-01-10 15:49:12 -0800

    Get react-pdf working to show resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,19 +1,17 @@
 import React from "react";
-import resumeImg from "../images/Resume.png"
+import resumePDF from "../images/Charlotte_Dibb_Resume.pdf"
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-// import { Document, Page, pdfjs } from "react-pdf";
-// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 export default function Resume() {
   return (
     <div>
-      <img style={{ width: "100%" }}
-        src={resumeImg}
-        alt="resume"
-      />
-      {/* <Document
-        // file="/CharlotteDibbResume.pdf"
-        // onLoadSuccess={this.onDocumentLoadSuccess}
-      ></Document> */}
+      <Document
+        file={resumePDF}
+      >
+        <Page pageNumber={1} />
+      </Document>
       <Link to="/">Back to homepage</Link>
     </div>
   );
