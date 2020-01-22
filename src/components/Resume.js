e40458345b7799d088bfff1a2456ce0c7fb78d2c
charcharmasonjar <charlotte.dibb@gmail.com>
2020-01-22 15:10:24 -0800

    Update imports for react-pdf

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,8 +1,9 @@
 import React from "react";
 import resumePDF from "../images/Charlotte_Dibb_Resume.pdf"
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Document, Page, pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import { Document, Page } from "react-pdf";
+// import { Document, Page, pdfjs } from "react-pdf";
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default function Resume() {
   return (
