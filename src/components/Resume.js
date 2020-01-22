e649661d5fc84cb71f045cdd614ad67d3674a29b
charcharmasonjar <charlotte.dibb@gmail.com>
2020-01-22 15:51:57 -0800

    Add stylesheet to allow links in pdf to display

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,16 +1,14 @@
 import React from "react";
-import resumePDF from "../images/Charlotte_Dibb_Resume.pdf"
+import resumePDF from "../images/Charlotte_Dibb_Resume.pdf";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Document, Page } from "react-pdf";
-// import { Document, Page, pdfjs } from "react-pdf";
-// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import 'react-pdf/dist/Page/AnnotationLayer.css';
 
 export default function Resume() {
+
   return (
     <div>
-      <Document
-        file={resumePDF}
-      >
+      <Document file={resumePDF}>
         <Page pageNumber={1} />
       </Document>
       <Link to="/">Back to homepage</Link>
