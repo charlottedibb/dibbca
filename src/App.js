ddb7eaf3ab67cf57cfe888fa5900b794fab184d7
charcharmasonjar <charlotte.dibb@gmail.com>
2020-02-09 23:33:53 -0800

    Add react-media for different mobile layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import Media from "react-media";
 import Home from "./components/Home.js";
 import Resume from "./components/Resume.js";
 import "./styles/App.css";
@@ -7,7 +14,7 @@ import "./styles/landing.css";
 import "./styles/projects.css";
 import "./styles/skills.css";
 import "./styles/form.css";
-import "./styles/resume.css"
+import "./styles/resume.css";
 
 export default function App() {
   return (
@@ -17,8 +24,15 @@ export default function App() {
           <Route path="/resume">
             <Resume />
           </Route>
-          <Route path="/">
-            <Home />
+          <Route path="/mobile">
+            <div>hey i'm mobiiile</div>
+          </Route>
+          <Route exact path="/">
+            <Media query={{ maxWidth: 599 }}>
+              {screenIsSmall =>
+                screenIsSmall ? <Redirect to="/mobile" /> : <Home />
+              }
+            </Media>
           </Route>
         </Switch>
       </div>
