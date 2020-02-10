7fe7a82c13b8a1fb7c03f4de2addeab9b6c3e50e
charcharmasonjar <charlotte.dibb@gmail.com>
2020-02-09 22:55:33 -0800

    Remove unused react-router imports

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,6 +1,6 @@
 import React from "react";
 import resumePDF from "../images/Charlotte_Dibb_Resume.pdf";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Link } from "react-router-dom";
 import { Document, Page } from "react-pdf";
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 
