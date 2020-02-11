763bfddda197091d7241792d6e61784a3735124c
charcharmasonjar <charlotte.dibb@gmail.com>
2020-02-11 12:30:32 -0800

    Resume resizes on window width change

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import resumePDF from "../images/Charlotte_Dibb_Resume.pdf";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Document, Page } from "react-pdf";
-import 'react-pdf/dist/Page/AnnotationLayer.css';
+import "react-pdf/dist/Page/AnnotationLayer.css";
 
 export default function Resume() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const listener = () => {
+      setWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", listener);
+    return () => {
+      window.removeEventListener("resize", listener);
+    };
+  }, []);
 
   return (
     <div className="resume">
       <Document file={resumePDF}>
-        <Page pageNumber={1} />
+        <Page pageNumber={1} width={width} />
       </Document>
-      <Link to="/" className="resume">Back to homepage</Link>
+      <Link to="/" className="resume">
+        Back to homepage
+      </Link>
     </div>
   );
 }
